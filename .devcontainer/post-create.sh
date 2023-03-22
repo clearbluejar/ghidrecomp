5ec4f0361303852387b043112f3a4dc085c98486
clearbluejar <3752074+clearbluejar@users.noreply.github.com>
2023-03-22 00:26:02 +0000

    remove ghidra_bridge, java build

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -5,12 +5,6 @@ source .env/bin/activate
 # Download latest pyi typings
 pip install ghidra-stubs
 
-# Install ghidra-bridge
-pip install ghidra_bridge
-
-# Install bridge scripts to local dir
-python -m ghidra_bridge.install_server .ghidra_bridge
-
 # Install pyhdira
 pip install pyhidra
 
@@ -18,15 +12,3 @@ pip install pyhidra
 if uname -a | grep -q 'aarch64'; then
     $GHIDRA_INSTALL_DIR/support/buildNatives
 fi
-
-# Setup Ghidra Dev for Reference
-git clone git@github.com:NationalSecurityAgency/ghidra.git ~/ghidra-master
-pushd ~/ghidra-master
-
-# Follow setup from https://github.com/NationalSecurityAgency/ghidra/blob/master/DevGuide.md
-gradle -I gradle/support/fetchDependencies.gradle init
-gradle prepdev
-
-popd
-
-echo 'To open up a Ghidra latest dev: code ~/ghidra-master'
